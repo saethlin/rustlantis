2d960850526f362b3316c89690aaa4e8624ea979
Ben Kimock <kimockb@gmail.com>
2026-07-02 23:40:03 -0400

    Add scripts

---
+++ fuzz.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+cargo build --release
+export MIRI_SYSROOT=$(cargo +nightly miri setup --print-sysroot)
+export RUSTLANTIS_MIRI_PATH="$(rustup run nightly miri --print sysroot)/bin/miri"
+export RUSTLANTIS_RUSTC_PATH="$(rustup run nightly rustc --print sysroot)/bin/rustc"
+
+function cleanup {
+    kill $(jobs -p)
+}
+trap cleanup EXIT
+
+echo 19 > /proc/self/autogroup
+proc=$(nproc)
+for job in $(seq 0 $((proc-1))); do
+    nice -n 19 ./job.sh &> $job.out &
+done
+
+wait
---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+while true
+do
+    seed=$(python3 -c "print(int.from_bytes(open('/dev/urandom', 'rb').read(8), 'little'))")
+    code=$(target/release/generate $seed)
+    if ! target/release/difftest - <<< $code
+    then
+        mkdir -p out
+        printf "%s" "$code" > out/$seed.rs
+    fi
+done
